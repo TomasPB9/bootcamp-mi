d5ba0a211aeb8706bd764e73e06ef54b3f3cbbc5
Tomas Bolio <tomas_9724@hotmail.com>
2021-03-15 19:31:01 -0600

    activity courseinfo

---
+++ courseinfo/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+//destructuracion
+const Title=({course})=> <h1>{course}</h1>
+
+//REPRESENTA LO MISMO
+
+// const Title=(props)=>{
+//   return <h1>{props.course}</h1>
+// }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Title course={course}/>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
